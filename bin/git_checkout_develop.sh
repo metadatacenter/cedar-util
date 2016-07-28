ddf6ce2dc5897ac9dbfca77dba5fcb8bd8424aaf
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2016-07-28 21:43:36 +0200

    Script to switch to develop branch on all repos

---
+++ bin/git_checkout_develop.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+clear
+echo ---------------------------------------------
+echo Switching and pulling develop branch
+echo ---------------------------------------------
+echo
+
+
+goauth
+git checkout develop
+git pull
+
+goconf
+git checkout develop
+git pull
+
+godocs
+git checkout develop
+git pull
+
+gofolder
+git checkout develop
+git pull
+
+goparent
+git checkout develop
+git pull
+
+goproject
+git checkout develop
+git pull
+
+gorepo
+git checkout develop
+git pull
+
+goresource
+git checkout develop
+git pull
+
+goschema
+git checkout develop
+git pull
+
+goserverutils
+git checkout develop
+git pull
+
+goeditor
+git checkout develop
+git pull
+
+gotemplate
+git checkout develop
+git pull
+
+goterminology
+git checkout develop
+git pull
+
+gouser
+git checkout develop
+git pull
+
+goutil
+git checkout develop
+git pull
+
+govaluerecommender
+git checkout develop
+git pull
+
+goadmintools
+git checkout develop
+git pull
+
+goprojectconfig
+git checkout develop
+git pull
+
