41b6640eb93ca25da80452922e166eaf85516daa
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2016-10-14 13:49:53 -0700

    Permission server renamed to groups server

--- bin/include-repo-list.sh
+++ bin/include-repo-list.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CEDAR_REPOS=( "cedar-admin-tools" "cedar-conf" "cedar-docs" "cedar-folder-server" "cedar-model" "cedar-parent" "cedar-permission-server" "cedar-project" "cedar-repo-server" "cedar-resource-server" "cedar-schema-server" "cedar-server-utils" "cedar-template-editor" "cedar-template-server" "cedar-terminology-server" "cedar-user-server" "cedar-util" "cedar-valuerecommender-server")
+CEDAR_REPOS=( "cedar-admin-tools" "cedar-conf" "cedar-docs" "cedar-folder-server" "cedar-model" "cedar-parent" "cedar-group-server" "cedar-project" "cedar-repo-server" "cedar-resource-server" "cedar-schema-server" "cedar-server-utils" "cedar-template-editor" "cedar-template-server" "cedar-terminology-server" "cedar-user-server" "cedar-util" "cedar-valuerecommender-server")
 
 echo List of CEDAR repos:
 for i in "${CEDAR_REPOS[@]}"
