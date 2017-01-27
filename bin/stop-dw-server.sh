8ec97d5cdb23689ed12830a2b7f934dc4980ac9b
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-01-27 14:12:57 -0800

    Use netcat or nc depending on os

--- bin/stop-dw-server.sh
+++ bin/stop-dw-server.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo "StopMe"$'\n'"stop" | netcat 127.0.0.1 $1
+`uname -a | grep -q 'Linux'`
+if [ $? = 0 ]; then
+	echo "Linux detected, using nc to stop Jetty" $1
+	echo "StopMe"$'\n'"stop" | nc 127.0.0.1 $1
+else
+	echo "Non-linux detected, using netcat to stop Jetty" $1
+	echo "StopMe"$'\n'"stop" | netcat 127.0.0.1 $1
+fi
