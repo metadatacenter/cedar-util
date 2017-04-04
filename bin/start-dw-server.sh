65026ac8ed520173d0d26fb657fdb17fab6c92f2
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-04-04 12:35:16 -0700

    No need to pass in keycloak.json

--- bin/start-dw-server.sh
+++ bin/start-dw-server.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #-verbose:class \
 java \
-  -Dkeycloak.config.path="$CEDAR_HOME/keycloak.json" \
   -DSTOP.PORT=$2 -DSTOP.KEY="StopMe" \
   -jar $CEDAR_HOME/cedar-$1-server/cedar-$1-server-application/target/cedar-$1-server-application-*.jar \
   server \
