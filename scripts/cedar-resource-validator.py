c44a7a82a2c7d583dfcead0b78384d79c12fcfb7
johardi <josef.hardi@stanford.edu>
2017-04-11 11:46:34 -0700

    Add simple validation reporting

--- scripts/cedar-resource-validator.py
+++ scripts/cedar-resource-validator.py
@@ -2,6 +2,7 @@ import argparse
 import json
 from urllib.parse import quote
 from cedar.utils import downloader, validator
+from collections import defaultdict
 
 
 def main():
@@ -19,38 +20,42 @@ def main():
     api_key = args.apikey
     ids = args.identifiers
 
+    report = create_empty_report()
+
     for id in ids:
         if 'template' in id:
-            validate_template(api_key, server_address, id)
+            validate_template(api_key, server_address, id, report)
         elif 'element' in id:
-            validate_element(api_key, server_address, id)
+            validate_element(api_key, server_address, id, report)
         elif 'field' in id:
             pass
         elif 'instance' in id:
-            validate_instance(api_key, server_address, id)
+            validate_instance(api_key, server_address, id, report)
         else:
-            print("Unknown type of resource to validate: " + id)
+            report_unknown_type(id, report)
+
+    show(report)
 
 
-def validate_template(api_key, server_address, template_id):
+def validate_template(api_key, server_address, template_id, report):
     template = get_template(api_key, server_address, template_id)
     request_url = server_address + "/command/validate?resource_types=template"
-    message = validator.validate_template(api_key, template, request_url=request_url)
-    printout(message)
+    status_code, message = validator.validate_template(api_key, template, request_url=request_url)
+    consume(report, status_code=status_code, output_message=message, resource_id=template_id)
 
 
-def validate_element(api_key, server_address, element_id):
+def validate_element(api_key, server_address, element_id, report):
     element = get_element(api_key, server_address, element_id)
     request_url = server_address + "/command/validate?resource_types=element"
-    message = validator.validate_element(api_key, element, request_url=request_url)
-    printout(message)
+    status_code, message = validator.validate_element(api_key, element, request_url=request_url)
+    consume(report, status_code=status_code, output_message=message, resource_id=element_id)
 
 
-def validate_instance(api_key, server_address, instance_id):
+def validate_instance(api_key, server_address, instance_id, report):
     instance = get_instance(api_key, server_address, instance_id)
     request_url = server_address + "/command/validate?resource_types=instance"
-    message = validator.get_resource(api_key, instance, request_url=request_url)
-    printout(message)
+    status_code, message = validator.get_resource(api_key, instance, request_url=request_url)
+    consume(report, status_code=status_code, output_message=message, resource_id=instance_id)
 
 
 def get_template(api_key, server_address, template_id):
@@ -80,20 +85,55 @@ def get_server_address(server):
     return server_address
 
 
+def create_empty_report():
+    return defaultdict(list)
+
+
 def escape(s):
-    return quote(s, safe='')
+    return quote(str(s), safe='')
 
 
-def to_json(obj):
-    return json.loads(json.dumps(obj))
+def to_json_string(obj, pretty=True):
+    if pretty:
+        return json.dumps(obj, indent=2, sort_keys=True)
+    else:
+        return json.dumps(obj)
 
 
-def printout(message):
-    is_valid = to_json(message)["validates"]
-    char = "."
-    if is_valid == 'false':
-        char = "*"
-    print(char, end="")
+def consume(report, **kwargs):
+    status_code = kwargs["status_code"]
+    output_message = kwargs["output_message"]
+    resource_id = kwargs["resource_id"]
+
+    indicator = "."  # means OK
+    if status_code > 200:
+        error_message = str(status_code) + " " + output_message["status"]
+        if output_message["message"]:
+            error_message += " - " + output_message["message"][:80] + "..."
+        report[error_message].append(resource_id)
+        indicator = "X"  # means server error
+    else:
+        is_valid = output_message["validates"]
+        if is_valid == 'false':
+            error_message = ', '.join(output_message["errors"]) # flatten list
+            report[error_message].append(resource_id)
+            indicator = "*"  # means validation error
+
+    print(indicator, end="", flush=True)
+
+
+def show(report):
+    report_size = len(report)
+    message = "No error was found."
+    if report_size > 0:
+        message = "Found " + str(report_size) + " validation error(s)."
+        message += "\n"
+        message += "Details: " + to_json_string(dict(report))
+    print("\r" + message)
+
+
+def report_unknown_type(resource_id, report):
+    report["Unknown type of resource to validate"].append(resource_id)
 
 
 if __name__ == "__main__":
--- scripts/cedar-validator.py
+++ scripts/cedar-validator.py
@@ -2,6 +2,7 @@ import argparse
 import json
 from urllib.parse import quote
 from cedar.utils import downloader, validator, enumerator
+from collections import defaultdict
 
 
 def main():
@@ -25,44 +26,45 @@ def main():
     limit = args.limit
     api_key = args.apikey
 
+    report = create_empty_report()
+
     if type == 'template':
-        validate_template(api_key, server_address, limit)
+        validate_template(api_key, server_address, limit, report)
     elif type == 'element':
-        validate_element(api_key, server_address, limit)
+        validate_element(api_key, server_address, limit, report)
     elif type == 'field':
         pass
     elif type == 'instance':
-        validate_instance(api_key, server_address, limit)
-    else:
-        print("Unknown type of resource to validate: " + type)
+        validate_instance(api_key, server_address, limit, report)
+
+    show(report)
 
 
-def validate_template(api_key, server_address, limit):
+def validate_template(api_key, server_address, limit, report):
     template_ids = get_template_ids(api_key, server_address, limit)
     for template_id in template_ids:
         template = get_template(api_key, server_address, template_id)
         request_url = server_address + "/command/validate?resource_types=template"
         status_code, message = validator.validate_template(api_key, template, request_url=request_url)
-        print(status_code, message)
-        printout(status_code, message)
+        consume(report, status_code=status_code, output_message=message, resource_id=template_id)
 
 
-def validate_element(api_key, server_address, limit):
+def validate_element(api_key, server_address, limit, report):
     element_ids = get_element_ids(api_key, server_address, limit)
     for element_id in element_ids:
         element = get_element(api_key, server_address, element_id)
         request_url = server_address + "/command/validate?resource_types=element"
         status_code, message = validator.validate_element(api_key, element, request_url=request_url)
-        printout(status_code, message)
+        consume(report, status_code=status_code, output_message=message, resource_id=element_id)
 
 
-def validate_instance(api_key, server_address, limit):
+def validate_instance(api_key, server_address, limit, report):
     instance_ids = get_instance_ids(api_key, server_address, limit)
     for instance_id in instance_ids:
         instance = get_instance(api_key, server_address, instance_id)
         request_url = server_address + "/command/validate?resource_types=instance"
         status_code, message = validator.validate_instance(api_key, instance, request_url=request_url)
-        printout(status_code, message)
+        consume(report, status_code=status_code, output_message=message, resource_id=instance_id)
 
 
 def get_element_ids(api_key, server_address, limit):
@@ -107,24 +109,51 @@ def get_server_address(server):
     return server_address
 
 
+def create_empty_report():
+    return defaultdict(list)
+
+
 def escape(s):
     return quote(str(s), safe='')
 
 
-def to_json(obj):
-    return json.loads(json.dumps(obj))
-
-
-def printout(status_code, message):
-    char = "."
-    if status_code > 200:
-        char = "X"
-        print(message)
+def to_json_string(obj, pretty=True):
+    if pretty:
+        return json.dumps(obj, indent=2, sort_keys=True)
     else:
-        is_valid = to_json(message)["validates"]
+        return json.dumps(obj)
+
+
+def consume(report, **kwargs):
+    status_code = kwargs["status_code"]
+    output_message = kwargs["output_message"]
+    resource_id = kwargs["resource_id"]
+
+    indicator = "."  # means OK
+    if status_code > 200:
+        error_message = str(status_code) + " " + output_message["status"]
+        if output_message["message"]:
+            error_message += " - " + output_message["message"][:80] + "..."  # get a snippet
+        report[error_message].append(resource_id)
+        indicator = "X"  # means server error
+    else:
+        is_valid = output_message["validates"]
         if is_valid == 'false':
-            char = "*"
-    print(char, end="", flush=True)
+            error_message = ', '.join(output_message["errors"]) # flatten list
+            report[error_message].append(resource_id)
+            indicator = "*"  # means validation error
+
+    print(indicator, end="", flush=True)
+
+
+def show(report):
+    report_size = len(report)
+    message = "No error was found."
+    if report_size > 0:
+        message = "Found " + str(report_size) + " validation error(s)."
+        message += "\n"
+        message += "Details: " + to_json_string(dict(report))
+    print("\r" + message)
 
 
 if __name__ == "__main__":
