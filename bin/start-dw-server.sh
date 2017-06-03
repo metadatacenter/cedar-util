d7e015af1f7aaec5576dd21d7df712ad744760a6
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-06-02 17:17:41 -0700

    Refactor to facilitate dockerization

--- bin/start-dw-server.sh
+++ bin/start-dw-server.sh
@@ -6,6 +6,6 @@ echo - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 java \
   -jar $CEDAR_HOME/cedar-$1-server/cedar-$1-server-application/target/cedar-$1-server-application-*.jar \
   server \
-  "$CEDAR_HOME/cedar-$1-server/cedar-$1-server-application/config.yml"
+  "$CEDAR_HOME/cedar-$1-server/cedar-$1-server-application/src/main/resources/config.yml"
 echo --------------------------------------------------------------------------------
 echo
