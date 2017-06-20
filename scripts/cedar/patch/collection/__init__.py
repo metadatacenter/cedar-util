648461506d84f5e57f99b39112e96fa6c8e2dcf7
johardi <josef.hardi@stanford.edu>
2017-06-19 21:22:25 -0700

    Init all classes inside collection

---
+++ scripts/cedar/patch/collection/__init__.py
@@ -0,0 +1,22 @@
+from cedar.patch.collection.AddIdToPropertiesPatch import AddIdToPropertiesPatch
+from cedar.patch.collection.AddOrderToUiPatch import AddOrderToUiPatch
+from cedar.patch.collection.AddPropertyLabelsToUiPatch import AddPropertyLabelsToUiPatch
+from cedar.patch.collection.FillEmptyValuePatch import FillEmptyValuePatch
+from cedar.patch.collection.RecreateRequiredArrayPatch import RecreateRequiredArrayPatch
+from cedar.patch.collection.RemoveArrayDuplicatesPatch import RemoveArrayDuplicatesPatch
+from cedar.patch.collection.RemoveEnumFromOneOfPatch import RemoveEnumFromOneOfPatch
+from cedar.patch.collection.RemoveEnumFromTypePatch import RemoveEnumFromTypePatch
+from cedar.patch.collection.RemoveIdFromPropertiesPatch import RemoveIdFromPropertiesPatch
+from cedar.patch.collection.RemoveValueFromPropertiesPatch import RemoveValueFromPropertiesPatch
+
+__all__ = [
+    "AddIdToPropertiesPatch",
+    "AddOrderToUiPatch",
+    "AddPropertyLabelsToUiPatch",
+    "FillEmptyValuePatch",
+    "RecreateRequiredArrayPatch",
+    "RemoveArrayDuplicatesPatch",
+    "RemoveEnumFromOneOfPatch",
+    "RemoveEnumFromTypePatch",
+    "RemoveIdFromPropertiesPatch",
+    "RemoveValueFromPropertiesPatch"]
