0963ce008cdede109ed85958b0a882355a809e76
johardi <josef.hardi@stanford.edu>
2017-06-20 11:44:59 -0700

    Commit a patch to add the missing _valueConstraints

---
+++ scripts/cedar/patch/collection/AddValueConstraintsToPropertiesPatch.py
@@ -0,0 +1,43 @@
+import jsonpatch
+import re
+from cedar.patch import utils
+
+
+class AddValueConstraintsToPropertiesPatch(object):
+
+    def __init__(self):
+        self.description = "Adds the missing _valueConstraints in the user's property object"
+        self.since = "1.1.0"
+        self.path = None
+
+    def is_applied(self, error_description):
+        pattern = re.compile("object has missing required properties \(\[('.+',)*'_valueConstraints'(,'.+')*\]\) at (/properties/[^/]+)*/properties/[^/]+$")
+        if pattern.match(error_description):
+            self.path = utils.get_error_location(error_description)
+            return True
+        else:
+            return False
+
+    def apply(self, doc, path=None):
+        patch = self.get_json_patch(doc, path)
+        patched_doc = jsonpatch.JsonPatch(patch).apply(doc)
+        return patched_doc
+
+    def get_json_patch(self, doc, path=None):
+        if self.path is None and path is None:
+            raise Exception("The method required a 'path' location")
+
+        if path is not None:
+            self.path = path
+
+        patches = []
+        patch = {
+            "op": "add",
+            "value": {
+                "requiredValue": False
+            },
+            "path": self.path + "/_valueConstraints"
+        }
+        patches.append(patch)
+
+        return patches
