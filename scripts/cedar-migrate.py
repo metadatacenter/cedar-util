c4b90f2cb324555b36e750268e68df3d926befec
johardi <josef.hardi@stanford.edu>
2017-09-06 10:11:57 -0700

    Fix printing progress bar

--- scripts/cedar-migrate.py
+++ scripts/cedar-migrate.py
@@ -29,23 +29,32 @@ def migrate(source_server_address, target_server_address, source_api_key, target
         print("Copying template: " + template_id)
         template = get_template(source_server_address, source_api_key, template_id)
         store_template(target_server_address, target_api_key, template)
-        print("Copying instances:")
+
         instance_ids = get_instance_ids(source_server_address, source_api_key, template_id)
-        store_instances(target_server_address, target_api_key, instance_ids)
-    print("Copying elements:")
+        for counter, instance_id in enumerate(instance_ids, start=1):
+            print_progressbar(instance_id, "instance", counter, len(instance_ids))
+            instance = get_instance(source_server_address, source_api_key, instance_id)
+            store_instance(target_server_address, target_api_key, instance)
+
     element_ids = get_element_ids(source_server_address, source_api_key)
-    store_elements(element_ids)
+    for counter, element_id in enumerate(element_ids, start=1):
+        print_progressbar(element_id, "element", counter, len(element_ids))
+        element = get_element(source_server_address, source_api_key, element_id)
+        store_element(target_server_address, target_api_key, element)
 
 
 def get_template_ids(server_address, api_key):
+    print("Collecting all the templates...", end="\r")
     return searcher.search_templates(server_address, api_key)
 
 
 def get_element_ids(server_address, api_key):
+    print("Collecting all the elements...", end="\r")
     return searcher.search_elements(server_address, api_key)
 
 
 def get_instance_ids(server_address, api_key, template_id):
+    print("Collecting all the corresponding instances...", end="\r")
     return searcher.search_instances_of(server_address, api_key, template_id)
 
 
@@ -65,36 +74,23 @@ def store_template(server_address, api_key, template):
     storer.store_template(server_address, api_key, template, import_mode=True)
 
 
-def store_elements(server_address, api_key, element_ids):
-    total_elements = len(element_ids)
-    for counter, element_id in enumerate(element_ids, start=1):
-        print_progressbar(element_id, counter, total_elements)
-        element = get_element(server_address, api_key, element_id)
-        store_element(server_address, api_key, element)
-
-
 def store_element(server_address, api_key, element):
     storer.store_element(server_address, api_key, element, import_mode=True)
-
-
-def store_instances(server_address, api_key, instance_ids):
-    total_instances = len(instance_ids)
-    for counter, instance_id in enumerate(instance_ids, start=1):
-        print_progressbar(instance_id, counter, total_instances)
-        instance = get_instance(server_address, api_key, instance_id)
-        store_instance(server_address, api_key, instance)
 
 
 def store_instance(server_address, api_key, instance):
     storer.store_instance(server_address, api_key, instance, import_mode=True)
 
 
-def print_progressbar(resource_id, counter, total_count):
+def print_progressbar(resource_id, resource_type, counter, total_count):
     resource_hash = extract_resource_hash(resource_id)
     percent = 100 * (counter / total_count)
     filled_length = int(percent)
     bar = "#" * filled_length + '-' * (100 - filled_length)
-    print("\rCopying (%d/%d): |%s| %d%% Complete [%s]" % (counter, total_count, bar, percent, resource_hash), end='\r')
+    if (counter < total_count):
+        print("\rCopying %s (%d/%d): |%s| %d%% Complete [%s]" % (resource_type, counter, total_count, bar, percent, resource_hash), end="\r")
+    else:
+        print("\rCopying %s (%d/%d): |%s| %d%% Complete [%s]" % (resource_type, counter, total_count, bar, percent, resource_hash))
 
 
 def extract_resource_hash(resource_id):
