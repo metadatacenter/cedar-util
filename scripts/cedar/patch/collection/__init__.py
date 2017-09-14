e57e3870e59e262114f211d95fc085513ea47a32
johardi <josef.hardi@stanford.edu>
2017-09-13 17:52:46 -0700

    Added a patch to remove 'oslc' prefix from the @context properties of a template element

--- scripts/cedar/patch/collection/__init__.py
+++ scripts/cedar/patch/collection/__init__.py
@@ -31,6 +31,7 @@ from cedar.patch.collection.RemoveEnumFromOneOfPatch import RemoveEnumFromOneOfP
 from cedar.patch.collection.RemoveEnumFromTypePatch import RemoveEnumFromTypePatch
 from cedar.patch.collection.RemoveIdFromPropertiesPatch import RemoveIdFromPropertiesPatch
 from cedar.patch.collection.RemoveInstanceOfPatch import RemoveInstanceOfPatch
+from cedar.patch.collection.RemoveOslcFromElementContextPropertiesPatch import RemoveOslcFromElementContextPropertiesPatch
 from cedar.patch.collection.RemoveValueFromPropertiesPatch import RemoveValueFromPropertiesPatch
 from cedar.patch.collection.RemovePageFromInnerUiPatch import RemovePageFromInnerUiPatch
 from cedar.patch.collection.RemovePatternPropertiesPatch import RemovePatternPropertiesPatch
@@ -75,6 +76,7 @@ __all__ = [
     "RemoveEnumFromTypePatch",
     "RemoveIdFromPropertiesPatch",
     "RemoveInstanceOfPatch",
+    "RemoveOslcFromElementContextPropertiesPatch",
     "RemoveValueFromPropertiesPatch",
     "RemovePageFromInnerUiPatch",
     "RemovePatternPropertiesPatch",
