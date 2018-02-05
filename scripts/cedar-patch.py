aa379c3a170c2e7832a2d21bd28b68a384f24753
johardi <josef.hardi@stanford.edu>
2018-02-05 11:52:07 -0800

    Add a new argument to get resource to patch from a file

--- scripts/cedar-patch.py
+++ scripts/cedar-patch.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 from pymongo import MongoClient
 from pymongo.errors import OperationFailure
 from requests import HTTPError
@@ -52,6 +53,10 @@ def main():
                         required=False,
                         metavar="DBNAME",
                         help="set the MongoDB database name to store the patched resources")
+    parser.add_argument("--input-file",
+                        required=False,
+                        metavar="FILENAME",
+                        help="an input file containing the resource to patch")
     parser.add_argument("--commit",
                         required=False,
                         action="store_true",
@@ -84,6 +89,7 @@ def main():
     mongodb_conn = args.mongodb_connection
     source_db_name = args.input_mongodb
     patch_db_name = args.output_mongodb
+    source_file_name = args.input_file
     model_version = args.model_version
 
     commit_patching = args.commit
@@ -95,32 +101,41 @@ def main():
     server_address = get_server_address(args.server)
     cedar_api_key = args.validation_apikey
 
-    mongodb_client = setup_mongodb_client(mongodb_conn)
-    source_database = setup_source_database(mongodb_client, source_db_name)
-    patch_database = setup_patch_database(mongodb_client, patch_db_name)
-
-    if revert_patching:
-        perform_revert_patching(mongodb_client, patch_db_name)
-    else:
-        patch_engine = build_patch_engine()
-        if resource_type == 'all':
-            template_ids = get_template_ids(None, source_database, limit)
-            patch_template(patch_engine, template_ids, source_database, model_version, output_dir, patch_database, output_partial_patch, debug)
-            element_ids = get_element_ids(None, source_database, limit)
-            patch_element(patch_engine, element_ids, source_database, model_version, output_dir, patch_database, output_partial_patch, debug)
-            instance_ids = get_instance_ids(None, source_database, limit)
-            patch_instance(instance_ids, source_database, output_dir, patch_database, debug)
-        elif resource_type == 'template':
-            template_ids = get_template_ids(lookup_file, source_database, limit)
-            patch_template(patch_engine, template_ids, source_database, model_version, output_dir, patch_database, output_partial_patch, debug)
+    patch_engine = build_patch_engine()
+    if source_file_name is not None:
+        if resource_type == 'template':
+            patch_template_file(patch_engine, source_file_name, model_version, output_dir, output_partial_patch, debug)
         elif resource_type == 'element':
-            element_ids = get_element_ids(lookup_file, source_database, limit)
-            patch_element(patch_engine, element_ids, source_database, model_version, output_dir, patch_database, output_partial_patch, debug)
+            patch_element_file(patch_engine, source_file_name, model_version, output_dir, output_partial_patch, debug)
         elif resource_type == 'field':
             pass
         elif resource_type == 'instance':
-            instance_ids = get_instance_ids(lookup_file, source_database, limit)
-            patch_instance(instance_ids, source_database, output_dir, patch_database, debug)
+            patch_instance_file(source_file_name, output_dir, debug)
+    elif mongodb_conn is not None and source_db_name is not None:
+        mongodb_client = setup_mongodb_client(mongodb_conn)
+        source_database = setup_source_database(mongodb_client, source_db_name)
+        patch_database = setup_patch_database(mongodb_client, patch_db_name)
+        if revert_patching:
+            perform_revert_patching(mongodb_client, patch_db_name)
+        else:
+            if resource_type == 'all':
+                template_ids = get_template_ids(None, source_database, limit)
+                patch_template(patch_engine, template_ids, source_database, model_version, output_dir, patch_database, output_partial_patch, debug)
+                element_ids = get_element_ids(None, source_database, limit)
+                patch_element(patch_engine, element_ids, source_database, model_version, output_dir, patch_database, output_partial_patch, debug)
+                instance_ids = get_instance_ids(None, source_database, limit)
+                patch_instance(instance_ids, source_database, output_dir, patch_database, debug)
+            elif resource_type == 'template':
+                template_ids = get_template_ids(lookup_file, source_database, limit)
+                patch_template(patch_engine, template_ids, source_database, model_version, output_dir, patch_database, output_partial_patch, debug)
+            elif resource_type == 'element':
+                element_ids = get_element_ids(lookup_file, source_database, limit)
+                patch_element(patch_engine, element_ids, source_database, model_version, output_dir, patch_database, output_partial_patch, debug)
+            elif resource_type == 'field':
+                pass
+            elif resource_type == 'instance':
+                instance_ids = get_instance_ids(lookup_file, source_database, limit)
+                patch_instance(instance_ids, source_database, output_dir, patch_database, debug)
 
         if not debug:
             show_report()
@@ -196,6 +211,36 @@ def build_patch_engine_v130():
     return patch_engine
 
 
+def patch_template_file(patch_engine, source_file_name, model_version, output_dir, output_partial_patch=False, debug=False):
+    try:
+        template = get_template_from_file(source_file_name)
+        template_id = template["@id"]
+        is_success, patched_template = patch_engine.execute(template, validate_template_callback, debug=debug)
+
+        if model_version:
+            set_model_version(patched_template, model_version)
+
+        if is_success:
+            if patched_template is not None:
+                create_report("resolved", template_id)
+                if output_dir is not None:
+                    filename = create_filename_from_id(template_id, prefix="template-patched-")
+                    write_to_file(patched_template, filename, output_dir)
+        else:
+            create_report("unresolved", template_id)
+            if output_partial_patch:
+                if output_dir is not None:
+                    filename = create_filename_from_id(template_id, prefix="template-partially-patched-")
+                    write_to_file(patched_template, filename, output_dir)
+            else:
+                if output_dir is not None:
+                    filename = create_filename_from_id(template_id, prefix="template-original-")
+                    write_to_file(template, filename, output_dir)
+    except (HTTPError, KeyError) as error:
+        create_report("error", [template_id, "Error details: " + str(error)])
+    print()  # console printing separator
+
+
 def patch_template(patch_engine, template_ids, source_database, model_version=None, output_dir=None, patch_database=None,
                    output_partial_patch=False, debug=False):
     total_templates = len(template_ids)
@@ -204,7 +249,7 @@ def patch_template(patch_engine, template_ids, source_database, model_version=No
             print_progressbar(template_id, counter, total_templates)
         try:
             template = get_template_from_mongodb(source_database, template_id)
-            is_success, patched_template = patch_engine.execute(template, validate_template, debug=debug)
+            is_success, patched_template = patch_engine.execute(template, validate_template_callback, debug=debug)
 
             if model_version:
                 set_model_version(patched_template, model_version)
@@ -236,11 +281,40 @@ def patch_template(patch_engine, template_ids, source_database, model_version=No
     print()  # console printing separator
 
 
-def validate_template(template):
+def validate_template_callback(template):
     is_valid, message = validator.validate_template(server_address, cedar_api_key, template)
     return is_valid, [error_detail["message"] + " at " + error_detail["location"]
                       for error_detail in message["errors"]
                       if not is_valid]
+
+
+def patch_element_file(patch_engine, source_file_name, model_version, output_dir, output_partial_patch, debug):
+    try:
+        element = get_element_from_file(source_file_name)
+        element_id = element["@id"]
+        is_success, patched_element = patch_engine.execute(element, validate_element_callback, debug=debug)
+
+        if model_version:
+            set_model_version(patched_element, model_version)
+
+        if is_success:
+            if patched_element is not None:
+                create_report("resolved", element_id)
+                if output_dir is not None:
+                    filename = create_filename_from_id(element_id, prefix="element-patched-")
+                    write_to_file(patched_element, filename, output_dir)
+        else:
+            create_report("unresolved", element_id)
+            if output_partial_patch:
+                if output_dir is not None:
+                    filename = create_filename_from_id(element_id, prefix="element-partially-patched-")
+                    write_to_file(patched_element, filename, output_dir)
+            else:
+                if output_dir is not None:
+                    filename = create_filename_from_id(element_id, prefix="element-original-")
+                    write_to_file(element, filename, output_dir)
+    except (HTTPError, KeyError, TypeError) as error:
+        create_report("error", [element_id, "Error details: " + str(error)])
 
 
 def patch_element(patch_engine, element_ids, source_database, model_version=None, output_dir=None, patch_database=None,
@@ -251,7 +325,7 @@ def patch_element(patch_engine, element_ids, source_database, model_version=None
             print_progressbar(element_id, counter, total_elements)
         try:
             element = get_element_from_mongodb(source_database, element_id)
-            is_success, patched_element = patch_engine.execute(element, validate_element, debug=debug)
+            is_success, patched_element = patch_engine.execute(element, validate_element_callback, debug=debug)
 
             if model_version:
                 set_model_version(patched_element, model_version)
@@ -283,11 +357,29 @@ def patch_element(patch_engine, element_ids, source_database, model_version=None
     print()  # console printing separator
 
 
-def validate_element(element):
+def validate_element_callback(element):
     is_valid, message = validator.validate_element(server_address, cedar_api_key, element)
     return is_valid, [error_detail["message"] + " at " + error_detail["location"]
                       for error_detail in message["errors"]
                       if not is_valid]
+
+
+def patch_instance_file(source_file_name, output_dir, debug=False):
+    print(" WARNING  | Patching the instances might still leave some errors. Please run the validator manually to check thoroughly")
+    try:
+        instance = get_instance_from_file(source_file_name)
+        instance_id = instance["@id"]
+        patched_instance = fix_context(instance)
+        patched_instance = rename(patched_instance, replace_valuelabel)
+
+        create_report("resolved", instance_id)
+
+        if output_dir is not None:
+            filename = create_filename_from_id(instance_id, prefix="instance-patched-")
+            write_to_file(patched_instance, filename, output_dir)
+    except (HTTPError, KeyError) as error:
+        create_report("error", [instance_id, "Error details: " + str(error)])
+    print()  # console printing separator
 
 
 def patch_instance(instance_ids, source_database, output_dir=None, mongo_database=None, debug=False):
@@ -500,6 +592,24 @@ def get_ids_from_file(filename):
     with open(filename) as infile:
         resource_ids = infile.readlines()
         return [id.strip() for id in resource_ids]
+
+
+def get_template_from_file(filename):
+    return read_file(filename)
+
+
+def get_element_from_file(filename):
+    return read_file(filename)
+
+
+def get_instance_from_file(filename):
+    return read_file(filename)
+
+
+def read_file(filename):
+    with open(filename) as infile:
+        content = json.load(infile)
+        return content
 
 
 def get_template_from_mongodb(source_database, template_id):
