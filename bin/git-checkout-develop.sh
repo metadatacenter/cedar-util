224a50cc310d9e43a7d70d7449a4da0f6b6188be
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2018-06-14 15:40:29 -0700

    Shell scripts modev to docker deploy

--- bin/git-checkout-develop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-clear
-echo ---------------------------------------------
-echo Switching and pulling develop branch
-echo ---------------------------------------------
-echo
-
-$CEDAR_HOME$1/cedar-util/bin/git-checkout-branch.sh develop
--- bin/include-repo-list.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-CEDAR_REPOS=( "cedar-admin-tool" "cedar-conf" "cedar-docker-build" "cedar-docker-deploy" "cedar-docs" "cedar-group-server" "cedar-keycloak-event-listener" "cedar-model-validation-library" "cedar-messaging-server" "cedar-parent" "cedar-project" "cedar-repo-server" "cedar-resource-server" "cedar-schema-server" "cedar-server-core-library" "cedar-swagger-ui" "cedar-template-editor" "cedar-template-server" "cedar-terminology-server" "cedar-user-server" "cedar-util" "cedar-valuerecommender-server" "cedar-worker-server" "cedar-submission-server" "cedar-workspace-server")
-
-echo List of CEDAR repos:
-for i in "${CEDAR_REPOS[@]}"
-do
-   echo "   - " $i
-done
