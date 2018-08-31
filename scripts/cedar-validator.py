48a2134788fc1e039487a6f3417d0219193236c1
johardi <josef.hardi@stanford.edu>
2018-08-31 15:11:45 -0700

    Minor fix an unused argument

--- scripts/cedar-validator.py
+++ scripts/cedar-validator.py
@@ -72,16 +72,16 @@ def main():
         mongodb_client = setup_mongodb_client(mongodb_conn)
         source_database = setup_source_database(mongodb_client, input_mongodb)
         if resource_type == 'template':
-            template_ids = get_template_ids(input_list, source_database, limit)
+            template_ids = get_template_ids(source_database, limit)
             validate_template_from_mongodb(template_ids, source_database)
         elif resource_type == 'element':
-            element_ids = get_element_ids(input_list, source_database, limit)
+            element_ids = get_element_ids(source_database, limit)
             validate_element_from_mongodb(element_ids, source_database)
         elif resource_type == 'field':
-            field_ids = get_field_ids(input_list, source_database, limit)
+            field_ids = get_field_ids(source_database, limit)
             validate_field_from_mongodb(field_ids, source_database)
         elif resource_type == 'instance':
-            instance_ids = get_instance_ids(input_list, source_database, limit)
+            instance_ids = get_instance_ids(source_database, limit)
             validate_instance_from_mongodb(instance_ids, source_database)
 
     show_report()
@@ -239,54 +239,50 @@ def validate_instance_from_mongodb(instance_ids, source_database):
             pass
 
 
-def get_template_ids(lookup_file, source_database, limit):
+def get_template_ids(source_database, limit):
     template_ids = []
-    if lookup_file is not None:
-        template_ids.extend(read_list(lookup_file))
+    if limit:
+        found_ids = source_database['templates'].distinct("@id").limit(limit)
+        template_ids.extend(found_ids)
     else:
-        if limit:
-            template_ids = source_database['templates'].distinct("@id").limit(limit)
-        else:
-            template_ids = source_database['templates'].distinct("@id")
+        found_ids = source_database['templates'].distinct("@id")
+        template_ids.extend(found_ids)
     filtered_ids = filter(lambda x: x is not None, template_ids)
     return list(filtered_ids)
 
 
-def get_element_ids(lookup_file, source_database, limit):
+def get_element_ids(source_database, limit):
     element_ids = []
-    if lookup_file is not None:
-        element_ids.extend(read_list(lookup_file))
+    if limit:
+        found_ids = source_database['template-elements'].distinct("@id").limit(limit)
+        element_ids.extend(found_ids)
     else:
-        if limit:
-            element_ids = source_database['template-elements'].distinct("@id").limit(limit)
-        else:
-            element_ids = source_database['template-elements'].distinct("@id")
+        found_ids = source_database['template-elements'].distinct("@id")
+        element_ids.extend(found_ids)
     filtered_ids = filter(lambda x: x is not None, element_ids)
     return list(filtered_ids)
 
 
-def get_field_ids(lookup_file, source_database, limit):
+def get_field_ids(source_database, limit):
     field_ids = []
-    if lookup_file is not None:
-        field_ids.extend(read_list(lookup_file))
+    if limit:
+        found_ids = source_database['template-fields'].distinct("@id").limit(limit)
+        field_ids.extend(found_ids)
     else:
-        if limit:
-            field_ids = source_database['template-fields'].distinct("@id").limit(limit)
-        else:
-            field_ids = source_database['template-fields'].distinct("@id")
+        found_ids = source_database['template-fields'].distinct("@id")
+        field_ids.extend(found_ids)
     filtered_ids = filter(lambda x: x is not None, field_ids)
     return list(filtered_ids)
 
 
-def get_instance_ids(lookup_file, source_database, limit):
+def get_instance_ids(source_database, limit):
     instance_ids = []
-    if lookup_file is not None:
-        instance_ids.extend(read_list(lookup_file))
+    if limit:
+        found_ids = source_database['template-instances'].distinct("@id").limit(limit)
+        instance_ids.extend(found_ids)
     else:
-        if limit:
-            instance_ids = source_database['template-instances'].distinct("@id").limit(limit)
-        else:
-            instance_ids = source_database['template-instances'].distinct("@id")
+        found_ids = source_database['template-instances'].distinct("@id")
+        instance_ids.extend(found_ids)
     filtered_ids = filter(lambda x: x is not None, instance_ids)
     return list(filtered_ids)
 
