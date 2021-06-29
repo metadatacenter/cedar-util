b8a7524a2a92c74285fb5b07c02670dcfe7b3d62
marcosmro <marcos.martinez.romero@gmail.com>
2021-06-28 22:45:39 -0700

    Share instances of a new version of the 'COVID Project Content' template with 'ZonMW COVID Metadata' group

--- cron/production/ccp-share-instances/share-instances.sh
+++ cron/production/ccp-share-instances/share-instances.sh
@@ -9,7 +9,9 @@ python3 /srv/cedar/cedar-util/scripts/python/group_permissions_to_instances.py h
 python3 /srv/cedar/cedar-util/scripts/python/group_permissions_to_instances.py https://repo.metadatacenter.org/templates/3258a592-a0a0-4822-9eda-5cd757e28926 https://repo.metadatacenter.net/groups/295c93d3-0c9b-430c-ba25-82f98ced501a read
 # Share instances of 'COVID Project Admin' template with 'ZonMW COVID Metadata' group
 python3 /srv/cedar/cedar-util/scripts/python/group_permissions_to_instances.py https://repo.metadatacenter.org/templates/eb3f3015-f776-4fc7-a074-7c113c4d0c0c https://repo.metadatacenter.org/groups/89ae032c-48e1-4448-8c42-ca39c72f900b read
+# Share instances of 'COVID Project Content' template (different versions/copies of it) with 'ZonMW COVID Metadata' group
 python3 /srv/cedar/cedar-util/scripts/python/group_permissions_to_instances.py https://repo.metadatacenter.org/templates/3258a592-a0a0-4822-9eda-5cd757e28926 https://repo.metadatacenter.org/groups/89ae032c-48e1-4448-8c42-ca39c72f900b read
 python3 /srv/cedar/cedar-util/scripts/python/group_permissions_to_instances.py https://repo.metadatacenter.org/templates/1c645834-e593-479e-9e2a-026b858cb58a https://repo.metadatacenter.org/groups/89ae032c-48e1-4448-8c42-ca39c72f900b read
+python3 /srv/cedar/cedar-util/scripts/python/group_permissions_to_instances.py https://repo.metadatacenter.org/templates/f697ab8d-7b71-4d48-9caf-8c256eb8ee13 https://repo.metadatacenter.org/groups/89ae032c-48e1-4448-8c42-ca39c72f900b read
 #echo "--------------------------------------------------------------------------"
 #echo "Script execution finished."
