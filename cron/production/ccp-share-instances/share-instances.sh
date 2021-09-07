dd679052de4e50af2c02f53f3ada7a3ab777191c
John Graybeal <jgraybeal@stanford.edu>
2021-09-07 11:57:59 -0700

    added templates per latest request

--- cron/production/ccp-share-instances/share-instances.sh
+++ cron/production/ccp-share-instances/share-instances.sh
@@ -9,10 +9,12 @@ python3 /srv/cedar/cedar-util/scripts/python/group_permissions_to_instances.py h
 python3 /srv/cedar/cedar-util/scripts/python/group_permissions_to_instances.py https://repo.metadatacenter.org/templates/3258a592-a0a0-4822-9eda-5cd757e28926 https://repo.metadatacenter.net/groups/295c93d3-0c9b-430c-ba25-82f98ced501a read
 # Share instances of 'COVID Project Admin' template with 'ZonMW COVID Metadata' group
 python3 /srv/cedar/cedar-util/scripts/python/group_permissions_to_instances.py https://repo.metadatacenter.org/templates/eb3f3015-f776-4fc7-a074-7c113c4d0c0c https://repo.metadatacenter.org/groups/89ae032c-48e1-4448-8c42-ca39c72f900b read
+python3 /srv/cedar/cedar-util/scripts/python/group_permissions_to_instances.py https://repo.metadatacenter.org/templates/337cb6f3-eef6-4b2f-9ffb-3f6d6cc9b9ac https://repo.metadatacenter.org/groups/89ae032c-48e1-4448-8c42-ca39c72f900b read
 # Share instances of 'COVID Project Content' template (different versions/copies of it) with 'ZonMW COVID Metadata' group
 python3 /srv/cedar/cedar-util/scripts/python/group_permissions_to_instances.py https://repo.metadatacenter.org/templates/3258a592-a0a0-4822-9eda-5cd757e28926 https://repo.metadatacenter.org/groups/89ae032c-48e1-4448-8c42-ca39c72f900b read
 python3 /srv/cedar/cedar-util/scripts/python/group_permissions_to_instances.py https://repo.metadatacenter.org/templates/1c645834-e593-479e-9e2a-026b858cb58a https://repo.metadatacenter.org/groups/89ae032c-48e1-4448-8c42-ca39c72f900b read
 python3 /srv/cedar/cedar-util/scripts/python/group_permissions_to_instances.py https://repo.metadatacenter.org/templates/f697ab8d-7b71-4d48-9caf-8c256eb8ee13 https://repo.metadatacenter.org/groups/89ae032c-48e1-4448-8c42-ca39c72f900b read
 python3 /srv/cedar/cedar-util/scripts/python/group_permissions_to_instances.py https://repo.metadatacenter.org/templates/8bcd40b3-ddb3-4b1f-af77-b3fe034c3db3 https://repo.metadatacenter.org/groups/89ae032c-48e1-4448-8c42-ca39c72f900b read
+python3 /srv/cedar/cedar-util/scripts/python/group_permissions_to_instances.py https://repo.metadatacenter.org/templates/908e33e2-9485-4a93-ab22-1688dc5819dc https://repo.metadatacenter.org/groups/89ae032c-48e1-4448-8c42-ca39c72f900b read
 #echo "--------------------------------------------------------------------------"
 #echo "Script execution finished."
