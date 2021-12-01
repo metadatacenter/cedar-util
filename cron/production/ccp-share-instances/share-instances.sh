f50a62c27d1cfa0ef1d85e3c9e47b4304455ed22
CEDAR app service account <cedar@cedr-prd-app-01.stanford.edu>
2021-11-30 16:16:52 -0800

    added templates per latest request

--- cron/production/ccp-share-instances/share-instances.sh
+++ cron/production/ccp-share-instances/share-instances.sh
@@ -16,5 +16,8 @@ python3 /srv/cedar/cedar-util/scripts/python/group_permissions_to_instances.py h
 python3 /srv/cedar/cedar-util/scripts/python/group_permissions_to_instances.py https://repo.metadatacenter.org/templates/f697ab8d-7b71-4d48-9caf-8c256eb8ee13 https://repo.metadatacenter.org/groups/89ae032c-48e1-4448-8c42-ca39c72f900b read
 python3 /srv/cedar/cedar-util/scripts/python/group_permissions_to_instances.py https://repo.metadatacenter.org/templates/8bcd40b3-ddb3-4b1f-af77-b3fe034c3db3 https://repo.metadatacenter.org/groups/89ae032c-48e1-4448-8c42-ca39c72f900b read
 python3 /srv/cedar/cedar-util/scripts/python/group_permissions_to_instances.py https://repo.metadatacenter.org/templates/908e33e2-9485-4a93-ab22-1688dc5819dc https://repo.metadatacenter.org/groups/89ae032c-48e1-4448-8c42-ca39c72f900b read
+python3 /srv/cedar/cedar-util/scripts/python/group_permissions_to_instances.py https://repo.metadatacenter.org/templates/28d58a30-1a42-4715-a742-d2f46690563e https://repo.metadatacenter.org/groups/89ae032c-48e1-4448-8c42-ca39c72f900b read
+python3 /srv/cedar/cedar-util/scripts/python/group_permissions_to_instances.py https://repo.metadatacenter.org/templates/22925909-9fb2-4ac8-a986-6db5ae7049e7 https://repo.metadatacenter.org/groups/89ae032c-48e1-4448-8c42-ca39c72f900b read
+python3 /srv/cedar/cedar-util/scripts/python/group_permissions_to_instances.py https://repo.metadatacenter.org/templates/de169781-7f75-4aef-a0cb-ac435fe3a4c7 https://repo.metadatacenter.org/groups/89ae032c-48e1-4448-8c42-ca39c72f900b read
 #echo "--------------------------------------------------------------------------"
 #echo "Script execution finished."
